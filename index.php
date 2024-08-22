fe1345a59b3c3addea7aeb2bce2d849af86ae447
DuongDucCuong <cuongbnkn2@gmail.com>
2024-08-22 15:03:50 +0700

    init project

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+$x = 3;
+switch ($x){
+    case 1:
+        echo "1";
+        break;
+    case 2:
+        echo "2";
+        break;
+    case 3:
+        echo "3";
+        break;
+        default:
+        echo "khong phai la so 1,2,3";
+}
+?>
